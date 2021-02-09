b4c90e0bcfca888af38b6b316377759c1192e74e
unknown <mehraj.malik007@gmail.com>
2021-02-09 16:53:22 +0530

    Arrays,forEach

--- index.js
+++ index.js
@@ -13,3 +13,31 @@ const person = {
 
 const {name, age, address: {code}} = person;
 console.log(`Name is ${name} and age is ${age} ${code}`);
+
+
+const todos = [
+    {
+        id:1,
+        text: "Meeting with boss",
+        isCompleted: false
+    },
+    {
+        id:2,
+        text: "Drink Water",
+        isCompleted: false
+    },
+    {
+        id:3,
+        text: "Buy Milk",
+        isCompleted: false
+    },
+    {
+        id:4,
+        text: "Call Tema",
+        isCompleted: true
+    }
+];
+
+todos.forEach(function(todo){
+    console.log(todo);
+});
