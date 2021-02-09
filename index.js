d727b293162a81c3c03db69f2383d7f77b4e376b
unknown <mehraj.malik007@gmail.com>
2021-02-09 17:08:31 +0530

    functions

--- index.js
+++ index.js
@@ -1,59 +1,8 @@
-const person = {
-    name: "Tom",
-    age: 10,
-    isBocked: false,
-    hobbies: ["Dance", "Singinging"],
-    address: {
-        city: "LA",
-        code: "12345"
-    }
-};
+function add(number1, number2) {
+    console.log("Addition is "+(number1+number2));
+}
 
-//console.log(person);
-
-const {name, age, address: {code}} = person;
-console.log(`Name is ${name} and age is ${age} ${code}`);
-
-
-const todos = [
-    {
-        id:1,
-        text: "Meeting with boss",
-        isCompleted: false
-    },
-    {
-        id:2,
-        text: "Drink Water",
-        isCompleted: false
-    },
-    {
-        id:3,
-        text: "Buy Milk",
-        isCompleted: false
-    },
-    {
-        id:4,
-        text: "Call Tema",
-        isCompleted: true
-    }
-];
-
-todos.forEach(function(todo){
-    console.log(todo);
-});
-
-
-// This will dynamically become an Array
-let allTodosText = todos.map(function(todo){
-    return todo.text
-});
-
-console.log(allTodosText);
-
-
-// This will dynamically become an Array
-let allCompletedTodos = todos.filter(function(todo){
-    return todo.isCompleted === true;
-});
-
-console.log(allCompletedTodos);
+add();
+add(1);
+add(1,2);
+add(1,2,3);
