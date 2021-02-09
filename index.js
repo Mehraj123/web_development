0543689bdabae21f7ed50755141cad1d7120ccf3
unknown <mehraj.malik007@gmail.com>
2021-02-09 18:12:12 +0530

    classes and methods with checks

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ class Person {
 const tom = new Person('Tom', 'Harry','07-07-1980');
 const jilly = new Person('Jilly', 'Ryan','07-07-1967');
 const unknown = new Person();
+const unknown1 = new Person('Herry','Bob','01-01-20001','something');
 
 console.log(tom);
 console.log(tom.getBirthYera());
@@ -27,3 +28,6 @@ console.log(jilly.getFullName());
 console.log(unknown);
 console.log(unknown.getBirthYera());
 console.log(unknown.getFullName());
+console.log(unknown1);
+console.log(unknown1.getBirthYera());
+console.log(unknown1.getFullName());
