ace4fee91a38e8bdf059198ccddbd4e87cae2b11
unknown <mehraj.malik007@gmail.com>
2021-02-09 18:09:28 +0530

    classes and methods

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const addNumbers = (number1, number2) => {
-    console.log("Addition is "+(number1+number2));
+class Person {
+    constructor(firstName, lastName, dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = new Date(dob);
+    }
+
+    getBirthYera() {
+       return this.dob.getFullYear();
+    }
+
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`
+    }
 }
 
-addNumbers();
-addNumbers(1);
-addNumbers(1,2);
-addNumbers(1,2,3);
+const tom = new Person('Tom', 'Harry','07-07-1980');
+const jilly = new Person('Jilly', 'Ryan','07-07-1967');
+const unknown = new Person();
+
+console.log(tom);
+console.log(tom.getBirthYera());
+console.log(tom.getFullName());
+console.log(jilly);
+console.log(jilly.getBirthYera());
+console.log(jilly.getFullName());
+console.log(unknown);
+console.log(unknown.getBirthYera());
+console.log(unknown.getFullName());
